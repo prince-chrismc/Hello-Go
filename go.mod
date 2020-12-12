933b313ff7b92026745b140cb2e1ffa9920805db
Chris McArthur <prince.chrismc@gmail.com>
2020-12-12 11:02:35 -0500

    calling module code
    https://golang.org/doc/tutorial/call-module-code

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module hello
 
 go 1.15
 
-require rsc.io/quote v1.5.2 // indirect
+replace greetings => ./greetings
+
+require (
+	greetings v0.0.0-00010101000000-000000000000
+	rsc.io/quote v1.5.2
+)
--- hello.go
+++ hello.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "rsc.io/quote"
 
-import "rsc.io/quote"
+    "greetings"
+)
 
 func main() {
+    fmt.Println(greetings.Hello("World"))
     fmt.Println(quote.Go())
 }
 
