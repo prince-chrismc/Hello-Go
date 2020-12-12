95c8c2e2889345a7b0c91e8956a66d4f29bf54ff
Chris Mc <prince.chrismc@gmail.com>
2020-12-12 11:24:06 -0500

    add IntMin with tests
    https://gobyexample.com/testing

--- hello.go
+++ hello.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "rsc.io/quote"
     "log"
+    "testing"
 
     "greetings"
 )
@@ -35,3 +36,41 @@ func main() {
     fmt.Println(message)
 }
 
+func IntMin(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func TestIntMinBasic(t *testing.T) {
+    ans := IntMin(2, -2)
+    if ans != -2 {
+
+        t.Errorf("IntMin(2, -2) = %d; want -2", ans)
+    }
+}
+
+func TestIntMinTableDriven(t *testing.T) {
+    var tests = []struct {
+        a, b int
+        want int
+    }{
+        {0, 1, 0},
+        {1, 0, 0},
+        {2, -2, -2},
+        {0, -1, -1},
+        {-1, 0, -1},
+    }
+
+    for _, tt := range tests {
+
+        testname := fmt.Sprintf("%d,%d", tt.a, tt.b)
+        t.Run(testname, func(t *testing.T) {
+            ans := IntMin(tt.a, tt.b)
+            if ans != tt.want {
+                t.Errorf("got %d, want %d", ans, tt.want)
+            }
+        })
+    }
+}
